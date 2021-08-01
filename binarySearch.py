f813fe0582fb9f4fd638209a99e684098ea3ab84
Lasha Suliashvili <58426811+kebechet-200@users.noreply.github.com>
2021-08-01 17:00:42 +0400

    add binary search algorithm

---
+++ binarySearch.py
@@ -0,0 +1,20 @@
+def bubbleSort(arr,n):
+    low = 0;
+    high = len(arr) - 1;
+    while (low<=high):
+        mid = (low + high) // 2
+        if (n == arr[mid]):
+            return mid ## this is not output, its index.
+        elif (n >= arr[mid]):
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1;
+
+def main():
+    arr = [4,5,6,7,8,9,10,11,12]
+    n = 11
+    print(bubbleSort(arr,n))
+
+if __name__ == "__main__":
+    main();
